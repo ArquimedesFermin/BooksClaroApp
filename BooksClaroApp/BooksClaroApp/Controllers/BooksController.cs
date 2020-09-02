569c7691680f193ce1ab3ec0b99878fa61df3fe7
ArquimedesFermin <arquimedesfermin17@gmail.com>
2020-09-02 17:51:03 -0400

    CRUDBooks

---
+++ BooksClaroApp/BooksClaroApp/Controllers/BooksController.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using BooksClaroApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BooksClaroApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        // GET: api/<BooksController>
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetBooks")]
+        public async Task<IEnumerable<Books>> Get()
+        {
+            var books = new List<Books>();
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri("https://fakerestapi.azurewebsites.net");
+
+            var url = "/api/Books";
+            HttpResponseMessage respuesta = await client.GetAsync(url);
+
+
+            if (respuesta.IsSuccessStatusCode)
+            {
+                var resp = await respuesta.Content.ReadAsStringAsync();
+                books = JsonConvert.DeserializeObject<List<Books>>(resp);
+            }
+            return books;
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        // GET api/<BooksController>/5
+        [HttpGet]
+        [Route("GetBooks/{id}")]
+
+        public async Task<Books> Get(int id)
+        {
+            var books = new Books();
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri("https://fakerestapi.azurewebsites.net");
+
+            var url = "/api/Books/" + id + "";
+            HttpResponseMessage respuesta = await client.GetAsync(url);
+
+
+            if (respuesta.IsSuccessStatusCode)
+            {
+                var resp = await respuesta.Content.ReadAsStringAsync();
+                books = JsonConvert.DeserializeObject<Books>(resp);
+            }
+
+            return books;
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="books"></param>
+        /// <returns></returns>
+        // POST api/<BooksController>
+        [HttpPost]
+        [Route("PostBooks")]
+        public async Task<HttpStatusCode> Post([FromBody] Books books)
+        {
+            var status = new HttpStatusCode();
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri("https://fakerestapi.azurewebsites.net");
+
+
+            var booksJson = JsonConvert.SerializeObject(books);
+            var data = new StringContent(booksJson, Encoding.UTF8, "application/json");
+
+            var url = "/api/Books/";
+            HttpResponseMessage respuesta = await client.PostAsync(url, data);
+
+            if (respuesta.IsSuccessStatusCode)
+            {
+                _ = respuesta.Content.ReadAsStringAsync();
+                await Task.CompletedTask;
+                status = respuesta.StatusCode;
+            }
+            else
+            {
+                status = respuesta.StatusCode;
+            }
+
+            return status;
+
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="books"></param>
+        /// <returns></returns>
+        // PUT api/<BooksController>/5
+        [HttpPut]
+        [Route("UpdateBooks/{id}")]
+        public async Task<HttpStatusCode> Put(int id, [FromBody] Books booksObject)
+        {
+            var books = new Books();
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri("https://fakerestapi.azurewebsites.net");
+
+            //Get
+            var urlGet = "/api/Books/" + id + "";
+            HttpResponseMessage respuestaGet = await client.GetAsync(urlGet);
+
+
+            if (respuestaGet.IsSuccessStatusCode)
+            {
+                var resp = await respuestaGet.Content.ReadAsStringAsync();
+                books = JsonConvert.DeserializeObject<Books>(resp);
+            }
+
+
+            books.ID = booksObject.ID;
+            books.Title = booksObject.Title;
+            books.Description = booksObject.Description;
+            books.PageCount = booksObject.PageCount;
+            books.Excerpt = booksObject.Excerpt;
+            books.PublishDate = booksObject.PublishDate;
+
+
+
+            //Update
+            var booksJson = JsonConvert.SerializeObject(books);
+            var data = new StringContent(booksJson, Encoding.UTF8, "application/json");
+
+            var urlUpdate = "/api/Books/" + id + "";
+            HttpResponseMessage respuestaUpdate = await client.PutAsync(urlUpdate, data);
+
+            HttpStatusCode status;
+            if (respuestaUpdate.IsSuccessStatusCode)
+            {
+                _ = respuestaUpdate.Content.ReadAsStringAsync();
+                await Task.CompletedTask;
+                status = respuestaUpdate.StatusCode;
+            }
+            else
+            {
+                status = respuestaUpdate.StatusCode;
+
+            }
+            return status;
+        }
+
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete]
+        [Route("DeleteBooks/{id}")]
+        public async Task<HttpStatusCode> Delete(int id)
+        {
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri("https://fakerestapi.azurewebsites.net");
+
+            var url = "/api/Books/" + id + "";
+            HttpResponseMessage respuesta = await client.DeleteAsync(url);
+
+
+            HttpStatusCode status;
+            if (respuesta.IsSuccessStatusCode)
+            {
+                _ = respuesta.Content.ReadAsStringAsync();
+                await Task.CompletedTask;
+                status = respuesta.StatusCode;
+            }
+            else
+            {
+                status = respuesta.StatusCode;
+
+            }
+            return status;
+
+        }
+    }
+}
---
+++ BooksClaroApp/BooksClaroApp/Models/Books.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksClaroApp.Models
+{
+    public class Books
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int PageCount { get; set; }
+        public string Excerpt { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
